fbf6e839e41c77c9eb8ce0744976a82159e97ec2
Agunbiade Abdulrasheed <79267850+agunbiade-ar@users.noreply.github.com>
2023-11-11 18:55:03 +0100

    switched from function constructor to class constructor

--- script.js
+++ script.js
@@ -27,164 +27,177 @@ let myLibrary = [
     pages: 407,
     isRead: true,
   },
-]
+];
 
-let cardsContainer = document.querySelector('.cards-container')
-let submit = document.querySelector('.dialog1-submit')
-let tableOfBooks = document.querySelector('.see-all-books')
+let cardsContainer = document.querySelector('.cards-container');
+let submit = document.querySelector('.dialog1-submit');
+let tableOfBooks = document.querySelector('.see-all-books');
 
 submit.addEventListener('click', function (e) {
-  e.preventDefault()
+  e.preventDefault();
 
-  let title = document.querySelector('#title').value
-  let author = document.querySelector('#author').value
-  let pages = document.querySelector('#pages').value
-  let isRead = document.querySelector('#isRead').checked
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let isRead = document.querySelector('#isRead').checked;
 
-  let requiredFields = document.querySelectorAll('input[required]')
-  let emptyFields = false
+  let requiredFields = document.querySelectorAll('input[required]');
+  let emptyFields = false;
 
   for (var i = 0; i < requiredFields.length; i++) {
     if (requiredFields[i].value.trim() === '') {
-      alert('Please fill in all required fields.')
-      emptyFields = true
-      break
+      alert('Please fill in all required fields.');
+      emptyFields = true;
+      break;
     }
   }
 
   if (!emptyFields) {
-    let book = new Book(title, author, pages, isRead)
-    let length = addBookToLibrary(book)
-    createCard(length, book)
-    dialog1.close()
+    let book = new Book(title, author, pages, isRead);
+    let length = addBookToLibrary(book);
+    createCard(length, book);
+    dialog1.close();
   }
-})
+});
 
-function Book(title, author, pages, isRead) {
-  this.author = author
-  this.title = title
-  this.pages = pages
-  this.isRead = isRead
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = isRead;
 
-  this.bookInfo = function () {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.isRead}`
+    this.bookInfo = function () {
+      return `${this.title} by ${this.author}, ${this.pages}, ${this.isRead}`;
+    };
   }
 }
 
+// function Book(title, author, pages, isRead) {
+//   this.author = author
+//   this.title = title
+//   this.pages = pages
+//   this.isRead = isRead
+
+//   this.bookInfo = function () {
+//     return `${this.title} by ${this.author}, ${this.pages}, ${this.isRead}`
+//   }
+// }
+
 function createCard(id, book) {
-  book.id = id
+  book.id = id;
 
-  let cardDiv = document.createElement('div')
-  let title = document.createElement('h5')
-  let hr = document.createElement('hr')
-  let author = document.createElement('h6')
-  let pages = document.createElement('h6')
-  let isReadBtn = document.createElement('button')
-  let removeBtn = document.createElement('button')
+  let cardDiv = document.createElement('div');
+  let title = document.createElement('h5');
+  let hr = document.createElement('hr');
+  let author = document.createElement('h6');
+  let pages = document.createElement('h6');
+  let isReadBtn = document.createElement('button');
+  let removeBtn = document.createElement('button');
 
-  cardDiv.classList.add('card', book.id)
-  title.className = 'title'
+  cardDiv.classList.add('card', book.id);
+  title.className = 'title';
 
-  isReadBtn.id = book.id
-  removeBtn.id = Math.pow(1.5, book.id)
+  isReadBtn.id = book.id;
+  removeBtn.id = Math.pow(1.5, book.id);
 
   isReadBtn.className = book.isRead
     ? 'button button-primary'
-    : 'button button-unread'
-  isReadBtn.textContent = book.isRead ? 'Read' : 'Unread'
+    : 'button button-unread';
+  isReadBtn.textContent = book.isRead ? 'Read' : 'Unread';
 
-  removeBtn.textContent = 'Remove Book'
-  removeBtn.className = 'button button-danger'
+  removeBtn.textContent = 'Remove Book';
+  removeBtn.className = 'button button-danger';
 
-  title.textContent = book.title.toUpperCase()
-  author.textContent = 'By: ' + book.author
-  pages.textContent = 'Pages ' + book.pages
+  title.textContent = book.title.toUpperCase();
+  author.textContent = 'By: ' + book.author;
+  pages.textContent = 'Pages ' + book.pages;
 
-  cardDiv.appendChild(title)
-  cardDiv.appendChild(hr)
-  cardDiv.appendChild(author)
-  cardDiv.appendChild(pages)
-  cardDiv.appendChild(isReadBtn)
-  cardDiv.appendChild(removeBtn)
-  cardsContainer.appendChild(cardDiv)
+  cardDiv.appendChild(title);
+  cardDiv.appendChild(hr);
+  cardDiv.appendChild(author);
+  cardDiv.appendChild(pages);
+  cardDiv.appendChild(isReadBtn);
+  cardDiv.appendChild(removeBtn);
+  cardsContainer.appendChild(cardDiv);
 
   cardsContainer.addEventListener('click', function (e) {
     //this forces type coercion
     if (e.target.id == book.id) {
-      book = toggleButton(book)
+      book = toggleButton(book);
       if (book.isRead) {
-        isReadBtn.classList.remove('button-unread')
-        isReadBtn.classList.add('button-primary')
-        isReadBtn.textContent = 'Read'
+        isReadBtn.classList.remove('button-unread');
+        isReadBtn.classList.add('button-primary');
+        isReadBtn.textContent = 'Read';
       } else {
-        isReadBtn.classList.remove('button-primary')
-        isReadBtn.classList.add('button-unread')
-        isReadBtn.textContent = 'Unread'
+        isReadBtn.classList.remove('button-primary');
+        isReadBtn.classList.add('button-unread');
+        isReadBtn.textContent = 'Unread';
       }
     }
 
     removeBtn.addEventListener('click', function () {
-      console.log(book.id)
-      myLibrary = deleteBookFromLibrary(book.id)
-      removeAllCards()
-      loadBooks()
-    })
-  })
+      console.log(book.id);
+      myLibrary = deleteBookFromLibrary(book.id);
+      removeAllCards();
+      loadBooks();
+    });
+  });
 }
 
 function deleteBookFromLibrary(cardId) {
-  return myLibrary.filter((book) => book.id !== cardId)
+  return myLibrary.filter((book) => book.id !== cardId);
 }
 
 function toggleButton(book) {
-  return { ...book, isRead: !book.isRead }
+  return { ...book, isRead: !book.isRead };
 }
 
 //returns length of library and uses it as the book id
 function addBookToLibrary(book) {
-  myLibrary.push(book)
-  return myLibrary.length
+  myLibrary.push(book);
+  return myLibrary.length;
 }
 
 function removeAllCards() {
-  let allCards = document.querySelectorAll('.card')
+  let allCards = document.querySelectorAll('.card');
   for (let i = 0; i < allCards.length; i++) {
-    cardsContainer.removeChild(allCards[i])
+    cardsContainer.removeChild(allCards[i]);
   }
 }
 
-const addBook = document.querySelector('.addBook')
+const addBook = document.querySelector('.addBook');
 
 addBook.addEventListener('click', function () {
-  const dialog = document.querySelector('.dialog1')
-  dialog.showModal() // Opens a modal
-})
+  const dialog = document.querySelector('.dialog1');
+  dialog.showModal(); // Opens a modal
+});
 
-const dialog1 = document.querySelector('.dialog1')
+const dialog1 = document.querySelector('.dialog1');
 dialog1.addEventListener('click', (e) => {
-  const dialogDimensions1 = dialog1.getBoundingClientRect()
+  const dialogDimensions1 = dialog1.getBoundingClientRect();
   if (
     e.clientX < dialogDimensions1.left ||
     e.clientX > dialogDimensions1.right ||
     e.clientY < dialogDimensions1.top ||
     e.clientY > dialogDimensions1.bottom
   ) {
-    dialog1.close()
+    dialog1.close();
   }
-})
+});
 
 function loadBooks() {
   myLibrary.forEach((book) => {
-    createCard(book.id, book)
-  })
+    createCard(book.id, book);
+  });
 }
 
-window.onload = loadBooks
+window.onload = loadBooks;
 
 document.querySelector('.reset-fields').addEventListener('click', function () {
-  document.querySelector('#title').focus()
-})
+  document.querySelector('#title').focus();
+});
 
 document.querySelector('.cancel').addEventListener('click', function () {
-  dialog1.close()
-})
+  dialog1.close();
+});
